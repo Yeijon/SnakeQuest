f0fb23f33438aaf5a1be9c7bcafcb48c6abe7854
Yeijon <yeijon04@gmail.com>
2024-05-03 16:53:28 +0800

    Day1 完成游戏开始界面

---
+++ GameBackground.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnakeQuest
+{
+    /// <summary>
+    /// GameBackground.xaml 的交互逻辑
+    /// </summary>
+    public partial class GameBackground : UserControl
+    {
+        public GameBackground()
+        {
+            InitializeComponent();
+
+            // 游戏界面图
+            ImageBrush img = new ImageBrush(
+                new BitmapImage(new Uri("/img/Snake_OG-logo.jpg",
+                UriKind.Relative)));
+            InitPicture.Background = img;
+        }
+    }
+}
